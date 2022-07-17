ff22d405ec6c01ca79a367fc006bc41d9d6fcf27
David O'Brien <ddrobrien@icloud.com>
2022-07-17 11:17:35 +0000

    javascript code includes overcoming empty value, -1 and greater than 9 issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,5 +19,15 @@ const result = document.querySelector(".lorem-text");
 form.addEventListener('submit', function(e){
     e.preventDefault();
     const value = parseInt (amount.value);
-    console.log(value);
-})
+    const random = Math.floor(Math.random() * text.length);
+    // empty value
+    // -1
+    // > 9
+if (isNaN(value) || value < 0 || value > 9) {
+    result.innerHTML = `<p class="result">${text[random]}</p>`;
+}
+else {
+    let tempText = text.slice(0,value);
+    console.log(tempText);
+}
+});
