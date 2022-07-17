653ad2110b57f51ee61f4cc98c72db06e76b0e92
David O'Brien <ddrobrien@icloud.com>
2022-07-17 11:25:30 +0000

    add number counter, and result to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,14 +20,19 @@ form.addEventListener('submit', function(e){
     e.preventDefault();
     const value = parseInt (amount.value);
     const random = Math.floor(Math.random() * text.length);
+
     // empty value
     // -1
     // > 9
-if (isNaN(value) || value < 0 || value > 9) {
+if (isNaN(value) || value <= 0 || value > 9) {
     result.innerHTML = `<p class="result">${text[random]}</p>`;
 }
 else {
     let tempText = text.slice(0,value);
-    console.log(tempText);
+    tempText = tempText.map(function(item){
+        return `<p class="result">${item}</p>`;
+    })
+    .join("");
+    result.innerHTML = tempText;
 }
 });
